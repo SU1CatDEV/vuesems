73d56eab0fc6d7baf41ef46e64c61e8e8169486c
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2024-06-30 16:27:43 +0700

    sem2

---
+++ src/blog.js
@@ -0,0 +1,75 @@
+new Vue({
+    el: "#app",
+    data: {
+        blogposts: [
+            {
+                title: "Создадим лучший макет перепланировки",
+                image: "../img/HomeBlog1.png",
+                label: "Дизайн кухни",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: ""
+            },
+            {
+                title: "Лучшие интерьерные идеи по низкой цене",
+                image: "../img/HomeBlog2.png",
+                label: "Дизайн для жизни",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: "labellong"
+            },
+            {
+                title: "Лучшие интерьерные решения для офисов",
+                image: "../img/HomeBlog3.png",
+                label: "Дизайн Интерьера",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: "labellong"
+            }
+        ],
+        blogposts2: [
+            {
+                title: "Создадим лучший макет перепланировки",
+                image: "../img/HomeBlog4.png",
+                label: "Дизайн кухни",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: ""
+            },
+            {
+                title: "Лучшие интерьерные идеи по низкой цене",
+                image: "../img/HomeBlog5.png",
+                label: "Дизайн для жизни",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: "labellong"
+            },
+            {
+                title: "Лучшие интерьерные решения для офисов",
+                image: "../img/HomeBlog6.png",
+                label: "Дизайн Интерьера",
+                date: {
+                    day: 26,
+                    month: "Декабрь",
+                    year: 2022
+                },
+                addclass: "labellong"
+            }
+        ]
+    },
+})
