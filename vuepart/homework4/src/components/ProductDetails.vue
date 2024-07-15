dbff91ca08b3464da13d080d9998a49504bd416c
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2024-07-15 13:50:11 +0700

    why would i bother with the seminar if i know how to solve the hw smh. also navcomponent and footercomponent already implemented cus i read minds lol (joke)

---
+++ vuepart/homework4/src/components/ProductDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <p>{{ formattedPrice }}</p>
+    <p>{{ product.available ? 'Available' : 'Out of stock' }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+  data () {
+    return {
+      product: {
+        name: 'Sample Product',
+        price: 99.99,
+        available: true
+      }
+    }
+  },
+  computed: {
+    formattedPrice () {
+      return `$${this.product.price.toFixed(2)}`
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
